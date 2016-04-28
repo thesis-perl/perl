7835e151b4a9049b853e5fb92e15de9c90d217f3
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2016-04-28 13:35:39 -0700

    [bug] fix sql schema bug

--- server/routes/studentSignupRoute.js
+++ server/routes/studentSignupRoute.js
@@ -4,9 +4,22 @@ var router = express.Router();
 // // load database module
 var knex = require('../db/db.js');
 
-router.post('/', function(req, res) {
+router.get('/', function (req, res) {
+  if(req.isAuthenticated()) {
+    res.redirect('/');
+  } else {
+    res.redirect('/student_login');
+  }
+})
+
+router.post('/', function (req, res) {
   var user = req.body;
   console.log(user);
+  var usernamePromise = null;
+  usernamePromise = knex.select('username').table('students');
+  // console.log('usernamePromise', usernamePromise);
+
+  return usernamePromise.then()
 })
 
 // export router
