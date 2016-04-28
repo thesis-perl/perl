8d081a6eec3ca821bcc116500a0165a58ec00b86
Ani Stepanyan <anisteffanie@gmail.com>
2016-04-28 14:59:30 -0700

    [create] create signup

--- client/app/services.js
+++ client/app/services.js
@@ -1,3 +1,33 @@
 angular.module('Perl.services', [])
+.factory('authFactory', function($http){
+
+   //signin
+//  var postSignin = function(username, password) {
+//    var user = {
+//    	 username: username,
+//    	 password: password
+//    };
+//    $http.post('/signin', user);
+// };
+
+  //signup request for student user
+ var postSignupStudent = function(userInfo){
+    return $http.post('/api/student_signup', userInfo);
+};
+
+ //signup request for tutor user
+ var postSignupTutor = function(userInfo){
+    $http.post('/api/tutor_signup', userInfo);
+};
+
+
+return {
+ 	//signin: postSignin,
+ 	studentSignup: postSignupStudent,
+ 	tutorSignup: postSignupTutor 
+ };
+
+
+});
 
 
