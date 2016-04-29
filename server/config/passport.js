4954e5bb34b1e77df9b98edba00f91db0ca5b3ed
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2016-04-29 16:28:14 -0700

    [feat] authentic login

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,15 +1,15 @@
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+// var passport = require('passport');
+// var LocalStrategy = require('passport-local').Strategy;
 
-// // load database module
-var knex = require('../db/db').knex;
+// // // load database module
+// var knex = require('../db/db').knex;
 
-module.exports = function(passport) {
-  passport.serializeUser(function(user, done) {
-    done(null, user.id);
-  })
+// module.exports = function(passport) {
+//   passport.serializeUser(function(user, done) {
+//     done(null, user.id);
+//   })
 
-  passport.deserializeUser(function(id, done) {
-    knex('tutors', 'students').select('username')
-  })
-}
+//   passport.deserializeUser(function(id, done) {
+//     knex('tutors', 'students').select('username')
+//   })
+// }
--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ var port = process.env.PORT || 8000;
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-var knex = require('knex').knex;
-var bcrypt = require('bcrypt-nodejs');
+var db = require('./db/db').knex;
+var bcrypt = require('bcrypt');
 var FirebaseTokenGenerator = require('firebase-token-generator');
 
 var key = require('./key');
@@ -26,12 +26,58 @@ app.use(express.static(__dirname + '/../client'));
 // app.use('/api/signup', require('./routes/signupRoute.js'));
 // app.use('/api/login', require('./routes/loginRoute.js'));
 
-app.post('/api/signin', function(req, res) {
+app.post('/api/login', function(req, res) {
   var user = req.body;
-  knex('users').where({username: user.username})
-    .then(function(resp) {
-      console.log(resp);
-    })
+  return new Promise(function(resolve) {
+    if (resolve) {
+      db('users').where('username', user.username)
+      .then(function(data) {
+        bcrypt.compare(user.password, data[0].password, function(err, result) {
+          console.log(user.password);
+          console.log(data[0].password);
+          if (err) {
+            res.sendStatus(401);
+          } else {
+            console.log(data)
+            var stringUID = user.username;
+            var token = tokenGenerator.createToken({ uid: stringUID, some: "arbitrary", data: "here"});
+            res.send({token: token});
+          }
+        })
+        resolve(data);
+      })
+    }
+  })
+})
+
+var saltRounds = 10;
+
+app.post('/api/signup', function(req, res) {
+  console.log(req.body);
+  var userPWbeforeEncrypt = req.body.password;
+  var user = req.body.username;
+  var hashedPW;
+  var userNameTaken = false;
+
+  return new Promise(function(resolve) {
+    if(resolve) {
+      db('users').where({username: user})
+      .then(function(data) {
+        if(data.length === 0) {
+          console.log("I have a user named: ", data);
+          bcrypt.hash(userPWbeforeEncrypt, saltRounds, function(err, hash) {
+            if(err) {
+              console.log(err);
+            } else {
+              db('users').insert({username: user, password: hash, location: req.body.location, bio: req.body.bio})
+              console.log("this is my req", req.body);
+            }
+          })
+        }
+        resolve(data);
+      })
+    }
+  })
 })
 
 // cors
