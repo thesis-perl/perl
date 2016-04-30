e53a81851b543741888cff0d13d6871534175604
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2016-04-30 12:06:32 -0700

    [feat] modularized api routing

--- server/routes/loginRoute.js
+++ server/routes/loginRoute.js
@@ -1,23 +1,46 @@
 var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcrypt-nodejs');
+var nodecrypt = require('bcrypt-nodejs');
+var FirebaseTokenGenerator = require('firebase-token-generator');
+var key = require('../key');
+var tokenGenerator = new FirebaseTokenGenerator(key.fireSecret);
+
 
 // load database module
 
-var knex = require('../db/db').knex;
-
-
-router.get('/', function(req, res) {
-  var user = req.body.username;
-  knex('users').where({username: user.username})
-    .then(function(resp) {
-      console.log(resp);
-    })
-})
+var db = require('../db/db').knex;
 
 router.post('/', function(req, res) {
-
-})
+  var user = req.body;
+  return new Promise(function(resolve) {
+    if (resolve) {
+      db('users').where('username', user.username)
+      .then(function(data) {
+        console.log('user password', user.password);
+        console.log('data password', data[0].password);
+        nodecrypt.compare(user.password, data[0].password, function(err, result) {
+          if (err) {
+            console.log('in err', err);
+            res.send(401);
+          } else {
+            if(result) {
+              console.log(data[0])
+              console.log(user)
+              var stringUID = user.username;
+              var token = tokenGenerator.createToken({ uid: stringUID });
+              res.send({token: token});
+            } else {
+              res.send("password don't match");
+            }
+          }
+        })
+      }).then(function(data) {
+        resolve(data);
+      })
+    }
+  })
+});
 
 // export router
 module.exports = router;
