4525a1b6e9abdb09945f121a8d1f41111240ae53
Zoe Chen <shzoechen@gmail.com>
2016-04-30 23:01:54 -0700

    fixed uploadfile bug

--- client/auth/auth.js
+++ client/auth/auth.js
@@ -114,3 +114,19 @@ angular.module('Perl.authentication', ['ngMaterial', 'firebase'])
   }
 
 }]) // end of authcontroller
+
+.directive('fileModel', ['$parse', function ($parse) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                var model = $parse(attrs.fileModel);
+                var modelSetter = model.assign;
+
+                element.bind('change', function(){
+                    scope.$apply(function(){
+                        modelSetter(scope, element[0].files[0]);
+                    });
+                });
+            }
+        };
+    }]);
