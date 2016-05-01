1fc43bb8910577b63e27a72c82aa0f1faff7d1ba
Jwing28 <jonathan.wing.lee@gmail.com>
2016-05-01 15:56:43 -0700

    [feat] add datepicker and time picker tutorprofile

--- client/student/tutorprofile.js
+++ client/student/tutorprofile.js
@@ -1,3 +1,34 @@
-angular.module('Perl.tutorProfile', [])
+angular.module('Perl.tutorProfile', [
+    "ngMaterial",
+    "ngAnimate",
+    "ngAria",
+    "ngMessages",
+    "mdPickers"])
 
-.controller('tutorProfile',function(){})
+.controller('tutorProfile', ['$scope', '$mdpDatePicker', '$mdpTimePicker' ,function($scope, $mdpDatePicker, $mdpTimePicker){
+  $scope.showDatePicker = function(ev) {
+    $mdpDatePicker($scope.currentDate, {
+      targetEvent: ev
+    }).then(function(selectedDate) {
+      $scope.currentDate = selectedDate;
+    });;
+  };
+  
+  $scope.filterDate = function(date) {
+    return moment(date).date() % 2 == 0;
+  };
+  
+  $scope.showTimePicker = function(ev) {
+    $mdpTimePicker($scope.currentTime, {
+      targetEvent: ev
+    }).then(function(selectedDate) {
+      $scope.currentTime = selectedDate;
+    });;
+  };
+
+  $scope.requestSession = function(){
+    
+  };    
+}]);
+
+
