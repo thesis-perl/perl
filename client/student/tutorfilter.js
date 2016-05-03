d6b80b2a350af2e687b3e4d34b776804845e4146
Zoe Chen <shzoechen@gmail.com>
2016-05-02 22:06:44 -0700

    [feat] display all tutors

--- client/student/tutorfilter.js
+++ client/student/tutorfilter.js
@@ -1,8 +1,16 @@
 angular.module('Perl.tutorFilter', [])
 
-.controller('tutorFilter',function($scope){
-	$scope.tutors;
+.controller('tutorFilter',function($scope, tutorFactory){
+	//need to send data of array of all tutors
+	//need to send tutors fullname
+	$scope.tutors = [];
 	$scope.getAllTutors = function() {
-		studentFactory.getAllTutors();
+		tutorFactory.getAllTutors()
+		.then(function(data){
+			$scope.tutors[0] = data.data;
+			console.log('tutors', $scope.tutors)
+		});
 	}
+
+	$scope.getAllTutors();
 })
