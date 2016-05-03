660fc34c192ddc3ed67dbd43ca0653fb8e2175db
Jwing28 <jonathan.wing.lee@gmail.com>
2016-05-03 13:50:53 -0700

    [feat] render tutor info on tutorprofile

--- client/landing/landing.js
+++ client/landing/landing.js
@@ -3,6 +3,8 @@ angular.module('Perl.landing', [
 ])
 
 
-.controller("landing", function(){
-
+.controller("landing", function($scope){
+  $scope.landingImage = {
+    'background-image': 'url(http://65.media.tumblr.com/96f9396713d30f7c9b08eb414643b574/tumblr_nqx91iffxy1tubinno1_1280.jpg)'
+  }
 })
--- client/student/tutorprofile.js
+++ client/student/tutorprofile.js
@@ -5,7 +5,21 @@ angular.module('Perl.tutorProfile', [
     "ngMessages",
     "mdPickers"])
 
-.controller('tutorProfile', ['$scope', '$mdpDatePicker', '$mdpTimePicker', '$stateParams',function($scope, $mdpDatePicker, $mdpTimePicker, $stateParams){
+.controller('tutorProfile', ['$scope', '$mdpDatePicker', '$mdpTimePicker', '$stateParams', '$location', 'studentFactory' ,function($scope, $mdpDatePicker, $mdpTimePicker, $stateParams, $location, studentFactory){
+
+  var tutorInfo = {
+    tutorId: $stateParams.id
+  }
+  studentFactory.getTutorInfo(tutorInfo).then(function(data){
+    var tutor = data.data;      
+    
+    $scope.name = tutor.fullname;
+    $scope.bio = tutor.bio;
+    $scope.loc = tutor.location;
+
+    //name, bio, location
+  });
+
   $scope.showDatePicker = function(ev) {
     $mdpDatePicker($scope.currentDate, {
       targetEvent: ev
@@ -26,11 +40,32 @@ angular.module('Perl.tutorProfile', [
     });;
   };
 
-  $scope.requestSession = function(){
-    
-  };  
+  $scope.redirectTutorDashboard = function() {
+    console.log('in redirectTutorDashboard')
+    // $location.url('./studentdashboard.html');
+  }
 
-  console.log('params', $stateParams.id)  //tutor's id
+  $scope.requestSession = function(){  
+  var studentInfo = JSON.parse(localStorage.getItem('userInfo'));
+  console.log(JSON.parse(localStorage.getItem('userInfo')).id);
+
+    // var dateTime = $scope.currentDate.toString();
+    // var date = dateTime.split("").slice(0,15).join(""); //ex. Mon May 02 2016
+    // var time = dateTime.split("").slice(16,21).join(""); //15:22
+    // var sessionInfo = {
+    //   date: date,
+    //   time: time
+    // }
+
+  if(!studentInfo.id){
+    console.log('student not signed in')
+  }
+
+    //LATER FOR WHEN FLAG INVITED IN DB
+    // studentFactory.getTutorInfo(tutorInfo).then(function(data){
+    //   console.log('Session requested, data received',data);
+    // });     
+  };    
 
 }]);
 
