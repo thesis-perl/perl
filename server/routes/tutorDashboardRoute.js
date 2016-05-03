29db835d05cdf44f35dabfc8661359c677fa02ef
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2016-05-03 11:23:22 -0700

    [update] polish api routes

--- server/routes/tutorDashboardRoute.js
+++ server/routes/tutorDashboardRoute.js
@@ -13,7 +13,7 @@ router.get('/invited', function(req, res) {
       for (var i=0; i<data.length; i++) {
         db('users').where({ id: data[i].sid })
         .then(function(data){
-          temp.push({id: data[0].id, username: data[0].username, bio: data[0].bio, location: data[0].loation, imgurl: data[0].imgurl, isTutor: data[0].isTutor, isStudent: data[0].isStudent, javascript: data[0].javascript, ruby: data[0].ruby, python: data[0].python});
+          temp.push({id: data[0].id, username: data[0].username, fullname: data[0].fullname, bio: data[0].bio, location: data[0].loation, imgurl: data[0].imgurl, isTutor: data[0].isTutor, isStudent: data[0].isStudent, javascript: data[0].javascript, ruby: data[0].ruby, python: data[0].python});
         })
         .then(function(){
           if (temp.length === data.length) {
@@ -37,7 +37,31 @@ router.get('/accepted', function(req, res) {
       for (var i=0; i<data.length; i++) {
         db('users').where({ id: data[i].sid })
         .then(function(data){
-          temp.push({id: data[0].id, username: data[0].username, bio: data[0].bio, location: data[0].loation, imgurl: data[0].imgurl, isTutor: data[0].isTutor, isStudent: data[0].isStudent, javascript: data[0].javascript, ruby: data[0].ruby, python: data[0].python});
+          temp.push({id: data[0].id, username: data[0].username, fullname: data[0].fullname, bio: data[0].bio, location: data[0].loation, imgurl: data[0].imgurl, isTutor: data[0].isTutor, isStudent: data[0].isStudent, javascript: data[0].javascript, ruby: data[0].ruby, python: data[0].python});
+        })
+        .then(function(){
+          if (temp.length === data.length) {
+            res.send(temp);
+          };
+        })
+      }
+    } else {
+      res.send(null);
+    }
+  })
+})
+
+router.get('/rejected', function(req, res) {
+  var id = req.headers.id;
+  var temp = [];
+
+  db('studentutor').where({ tid: id }).andWhere({ status: 3 })
+  .then(function(data) {
+    if (data[0]) {
+      for (var i=0; i<data.length; i++) {
+        db('users').where({ id: data[i].sid })
+        .then(function(data){
+          temp.push({id: data[0].id, username: data[0].username, fullname: data[0].fullname, bio: data[0].bio, location: data[0].loation, imgurl: data[0].imgurl, isTutor: data[0].isTutor, isStudent: data[0].isStudent, javascript: data[0].javascript, ruby: data[0].ruby, python: data[0].python});
         })
         .then(function(){
           if (temp.length === data.length) {
