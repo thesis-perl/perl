732b71bd8ceae43906c9f52b80d9678a5c18914b
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2016-05-04 19:59:52 -0700

    [feat] work on tutor side chat

--- server/socket/socket.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (io) {
-  io.on('connection', function (socket) {
-    console.log('in socket')
-    socket.broadcast.emit('user connected');
-    socket.on('code changed', function(code) {
-      io.sockets.emit('broadcast', code);
-    });
-  });
-};
