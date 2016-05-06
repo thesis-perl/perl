fdcc11e0f847e0c7c35cbab2213e571466759575
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2016-05-06 16:41:27 -0700

    [update] refine fav/unfav api routing

--- server/db/db.js
+++ server/db/db.js
@@ -15,18 +15,19 @@ var knex = require('knex') ({
 
 knex.schema.createTableIfNotExists('users', function (user) {
   user.increments('id').primary();
+  user.string('fullname').unique();
   user.string('username').unique();
   user.string('password').unique();
   user.string('bio');
   user.string('location');
   user.string('imgurl');
-  user.binary('online');
-  user.binary('isTutor');
-  user.binary('isStudent');
-  user.binary('javascript');
-  user.binary('ruby');
-  user.binary('python');
-  // for all binary, 0: false, 1: true
+  user.integer('online');
+  user.integer('isTutor');
+  user.integer('isStudent');
+  user.integer('javascript');
+  user.integer('ruby');
+  user.integer('python');
+  // for all integer, 0: false, 1: true
 }).then(function() {
   console.log('tutor table created');
 });
@@ -35,9 +36,9 @@ knex.schema.createTableIfNotExists('studentutor', function(join) {
   join.increments('ID').primary();
   join.integer('sid').unsigned();
   join.integer('tid').unsigned();
-  join.binary('status');
+  join.integer('status');
   // 0: favorited, but neither invited nor accepted, 1: invited, 2: accepted
-  join.binary('fav');
+  join.integer('fav');
   // 0: not favorited 1: favorited
   join.foreign('sid').references('id').inTable('users');
   join.foreign('tid').references('id').inTable('users');
--- server/routes/unfavRoute.js
+++ server/routes/unfavRoute.js
@@ -8,10 +8,17 @@ router.put('/', function(req, res) {
   var sid = req.body.sid;
   var tid = req.body.tid;
 
-  db('studentutor').where({sid: sid, tid: tid})
+  db('studentutor').where({sid: sid, tid: tid}).whereNot({status: 0})
   .update({fav: 0})
   .then(function(data){
-    console.log(sid + " unfavorited " + tid);
+    console.log('put', data);
+    res.sendStatus(sid + " unfavorited " + tid);
+  })
+
+  db('studentutor').where({sid: sid, tid: tid, status: 0})
+  .del()
+  .then(function(data){
+    res.sendStatus(sid + " del unfavorited " + tid);
   })
 })
 
