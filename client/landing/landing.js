42e223056fa6f13fc860925d7648bebb514ee963
Ani Stepanyan <anisteffanie@gmail.com>
2016-05-07 11:16:54 -0700

    [style]session and landing page styling progress

--- client/landing/landing.js
+++ client/landing/landing.js
@@ -1,10 +1,12 @@
 angular.module('Perl.landing', [
 	"moment-picker"
 ])
-
-
-.controller("landing", function($scope){
-  $scope.landingImage = {
-    'background-image': 'url(http://65.media.tumblr.com/96f9396713d30f7c9b08eb414643b574/tumblr_nqx91iffxy1tubinno1_1280.jpg)'
+.controller("landing", function($scope, $state){
+  $scope.redirectSignin = function(){
+	$state.go('signin');
+  }
+  
+  $scope.redirectSignup = function(){
+	$state.go('signup');
   }
 })
