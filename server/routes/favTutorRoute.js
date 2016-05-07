ad04a0e46b014146da6effb85f631c6efb6b9d01
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2016-05-06 18:50:28 -0700

    [feat] merge

--- server/routes/favTutorRoute.js
+++ server/routes/favTutorRoute.js
@@ -5,24 +5,26 @@ var router = express.Router();
 var db = require('../db/db').knex;
 
 router.post('/', function(req, res) {
- var sid = req.body.sid;
- var tid = req.body.tid;
- db('studentutor').where({sid: sid, tid: tid})
- .then(function(data) {
-   if (!data[0]) {
-     db('studentutor').insert({sid: sid, tid: tid, fav: 1, status: 0})
-     .then(function(data){
-       res.sendStatus(sid + " favorited " + tid);
-     })
-   } else {
-     db('studentutor').where({sid: sid, tid: tid})
-     .update({fav: 1})
-     .then(function(data){
-       res.sendStatus(sid + " updated " + tid + "to be favorite");
-     })
-   }
- })
-});
+
+  var sid = req.body.sid;
+  var tid = req.body.tid;
+  db('studentutor').where({sid: sid, tid: tid})
+  .then(function(data) {
+    if (!data[0]) {
+      db('studentutor').insert({sid: sid, tid: tid, fav: 1, status: 0})
+      .then(function(data){
+        res.sendStatus(sid + " favorited " + tid);
+      })
+    } else {
+      db('studentutor').where({sid: sid, tid: tid})
+      .update({fav: 1})
+      .then(function(data){
+        res.sendStatus(sid + " updated " + tid + "to be favorite");
+      })
+    }
+  })
+})
+
 
 // export router
 module.exports = router;
