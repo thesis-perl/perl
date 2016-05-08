3e7314fec21b942e070d55add11356521e6c2a80
Jwing28 <jonathan.wing.lee@gmail.com>
2016-05-07 17:00:15 -0700

    [create] create test to sign in and sign out

--- client/testing/signoutSpec.js
+++ client/testing/signoutSpec.js
@@ -1,4 +1,4 @@
-describe('Login', function() {
+describe('Login ', function() {
   it('should successfully sign a user in', function() {
     // navigates to the sign in page
     browser.ignoreSynchronization = true;
@@ -11,22 +11,28 @@ describe('Login', function() {
     // clicks the sign in button
     element(by.id('signInbtn')).click();
 
-    // waits for page to load and checks that the browser goes to the studentDashboard
+    // waits for page to load and checks that the browser goes to the profile page
     browser.sleep(2000);
 
-    // checks that the url is the studentDashboard
+    // checks that the url is the profile page
     expect(browser.getCurrentUrl()).toEqual('http://localhost:8000/#/studentDashboard');
   });
 
-  it('should allow signed in user to view list of tutor profiles', function () {
+  it('should allow signed in user to sign out', function () {
 
-    //click link to redirect to tutorfilter
-    element(by.css(".md-accent")).click();
+    //click button to show sidebar
+    element(by.css('[ng-click="$mdOpenMenu($event)"]')).click();
 
     // waits for page to load and checks that the browser goes to the tutorFilter page
     browser.sleep(2000);    
 
-    // checks that the url is the profile page
-    expect(browser.getCurrentUrl()).toEqual('http://localhost:8000/#/tutorFilter');    
+    //click button to sign out
+    element(by.css('[ng-click="signOutUser()"]')).click();
+
+    // waits for page to load and checks that the browser redirects to sign in
+    browser.sleep(2000);    
+
+    // checks that the url is the sign in page
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:8000/#/signin');    
   })
 });
