4f7cfe370411726a8a4ba3d6394de891c8249948
Jwing28 <jonathan.wing.lee@gmail.com>
2016-05-10 15:57:16 -0700

    [create] create invalid signin test

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['./client/testing/signoutSpec.js'],
+  specs: ['./client/testing/invalidsigninSpec.js'],
   capabilities: {
     browserName: 'firefox'
   }
