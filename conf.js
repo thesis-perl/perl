8fc92ba266b112ac612214078d5ed447a5488305
Jwing28 <jonathan.wing.lee@gmail.com>
2016-05-14 14:47:40 -0700

    [create] create tutorprofile test

--- conf.js
+++ conf.js
@@ -2,8 +2,8 @@
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['./client/testing/tutorsignupSpec.js'],
+  specs: ['./client/testing/tutorprofileSpec.js'],
   capabilities: {
-    browserName: 'firefox'
+    browserName: 'chrome'
   }
 }
