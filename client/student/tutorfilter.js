4be31a86dfc36c7c55109bba90a977c49a353c27
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2016-05-17 11:33:39 -0700

    [bug] fix tutor filter message button

--- client/student/tutorfilter.js
+++ client/student/tutorfilter.js
@@ -1,6 +1,6 @@
 angular.module('Perl.tutorFilter', [])
 
-.controller('tutorFilter',function($scope, tutorFactory){
+.controller('tutorFilter',function($scope, tutorFactory, $mdMedia, $mdDialog){
 
 	var user = JSON.parse(localStorage.getItem('userinfo'));
 	$scope.selectedItem;
@@ -39,6 +39,35 @@ angular.module('Perl.tutorFilter', [])
 	$scope.deleteFavorite = function(tid) {
 		tutorFactory.deleteFavorite(user.id, tid);
 	}
+
+	$scope.showAdvanced = function(ev) {
+    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+
+    $mdDialog.show({
+      controller: 'chat',
+      templateUrl: '../session/chat.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: useFullScreen
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+
+    $scope.$watch(function() {
+      return $mdMedia('xs') || $mdMedia('sm');
+    }, function(wantsFullScreen) {
+      $scope.customFullscreen = (wantsFullScreen === true);
+    });
+
+  };
+
+  $scope.getTutorId = function(tid) {
+    studentFactory.getTutorId(tid);
+  }
 
 	$scope.getAllTutors();
 })
--- server/routes/review.js
+++ server/routes/review.js
@@ -6,11 +6,11 @@ var db = require('../db/db').knex;
 
 router.get('/', function(req, res) {
   var tid = req.headers.tid;
-  db('reviews').where({tid, tid})
+  db('reviews').where({tid: tid})
   .rightJoin('users', 'reviews.sid', 'users.id')
   .then(function(data) {
     res.send(data);
-  }) 
+  })
 })
 
 router.post('/', function(req, res) {
